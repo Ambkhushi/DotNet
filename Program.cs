ecf2f915330ae64d718451d37f4b3c1f73302836
gsmca128@sgsitsindore.in <gsmca128@sgsitsindore.in>
2023-07-11 11:41:42 +0530

    second commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @out
+{
+    public class Calc
+    {
+        public void Add(params double[] numbers)
+        {
+            double sum = 0;
+            foreach(double number in numbers)
+            {
+                sum += number;
+            }
+            Console.WriteLine("Total of Number is : \t" + sum);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
